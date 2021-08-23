b7c71745fac25e510bd02afedac71f32b9efca2c
Crowdbotics <team@crowdbotics.com>
2021-08-23 21:23:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lokl_30054Identifier",
+  url: "https://lokl_30054Identifier.botics.co",
+  api: "https://lokl_30054Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
